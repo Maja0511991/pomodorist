4e02fc4b1f9823fef232a8b6e04f9e6ed9d26e31
Maja0511991 <sarahconor05@gmail.com>
2017-12-02 14:53:57 -0800

    project completed

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,1 +1,107 @@
 
+var secondsOnes = 0;
+var secondsTens = 0;
+var minutesOnes = 0;
+var minutesTens = 0;
+var timerOn = false;
+var timerJustStarted = true;
+
+
+
+    
+function countDown25()
+    {
+    if (timerOn === false) 
+        {
+        window.setInterval(subtractOne, 1000);
+        }
+    minutesTens = 2;
+    minutesOnes = 5;
+    secondsTens = 0;
+    secondsOnes = 0;
+    document.getElementById('seconds-ones').innerHTML=secondsOnes;
+    document.getElementById('seconds-tens').innerHTML=secondsTens;
+    document.getElementById('minutes-ones').innerHTML=minutesOnes;
+    document.getElementById('minutes-tens').innerHTML=minutesTens;
+        
+        
+    timerOn = true;
+    }
+    
+function countDown5()
+    {
+    if (timerOn === false)
+        {
+        window.setInterval(subtractOne, 1000);
+        }
+    minutesTens = 0;
+    minutesOnes = 5;
+    secondsTens = 0;
+    secondsOnes = 0;
+    document.getElementById('seconds-ones').innerHTML=secondsOnes;
+    document.getElementById('seconds-tens').innerHTML=secondsTens;
+    document.getElementById('minutes-ones').innerHTML=minutesOnes;
+    document.getElementById('minutes-tens').innerHTML=minutesTens;
+
+        
+    timerOn = true;
+    }
+
+function subtractOne()
+    {
+    
+    if (timerOn && minutesTens === 0 && minutesOnes === 0 && secondsTens === 0 && secondsOnes === 0)
+    {
+    window.alert("Time's up!");
+    refresh();
+    }
+    
+    else if (timerJustStarted && secondsTens === 0 && secondsOnes === 0 && minutesOnes === 0)
+        {
+        minutesOnes = 9;
+        secondsTens = 5;
+        secondsOnes = 9;
+        minutesOnes--
+        timerJustStarted = false;
+        }
+    else if (timerJustStarted && secondsTens === 0 && secondsOnes === 0 && minutesOnes !== 0)
+        {
+        secondsTens = 5;
+        secondsOnes = 9;
+        minutesOnes--
+        timerJustStarted = false;
+        }
+    
+    document.getElementById('seconds-ones').innerHTML=secondsOnes;
+    document.getElementById('seconds-tens').innerHTML=secondsTens;
+    document.getElementById('minutes-ones').innerHTML=minutesOnes;
+    document.getElementById('minutes-tens').innerHTML=minutesTens;
+    secondsToMinutes(); 
+    secondsOnes--;
+    }
+    
+function secondsToMinutes()
+    {
+    if (minutesOnes === 0 && secondsOnes === 0 && secondsTens === 0)
+        {
+        minutesTens--;
+        minutesOnes = 10;
+        }
+    if (secondsTens === 0 && secondsOnes === 0)
+        {
+        minutesOnes--;
+        secondsTens = 6;
+        }
+    if ( secondsOnes === 0)
+        {
+        secondsTens--;
+        secondsOnes = 10;
+        }
+    }
+   
+function refresh()
+    {
+    location.reload()
+    }
+    
+    
